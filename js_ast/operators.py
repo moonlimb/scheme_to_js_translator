f1fa5ef1ec1ff97bb78b874970ac9a172c099066
Moon Limb <moonlimb@gmail.com>
2012-11-13 23:26:15 -0800

    wrote my first JS output- basic calc functions

--- js_ast/operators.py
+++ js_ast/operators.py
@@ -8,5 +8,5 @@
 
 block_type ={'procedures_defreturn': 'function', 'math_arithmetic':'', 'variables_get':''}
 
-basic = {'ADD': ' + ', 'MULTIPLY': ' * ', 'DIVIDE':' / ', 'POWER':' ** '}
+basic = {'ADD': ' + ', 'MINUS': ' - ', 'MULTIPLY': ' * ', 'DIVIDE':' / ', 'POWER':' ** '}
 
